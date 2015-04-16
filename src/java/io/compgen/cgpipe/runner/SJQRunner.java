b8e94bd822464bfcd4b90747a679a3d38042b658
Marcus Breese <mbreese@stanford.edu>
2015-04-16 12:00:58 -0700

    code cleanup

--- src/java/io/compgen/cgpipe/runner/SJQRunner.java
+++ src/java/io/compgen/cgpipe/runner/SJQRunner.java
@@ -14,7 +14,6 @@ import io.compgen.sjq.server.SJQServerException;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
