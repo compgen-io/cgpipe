e6346e2db911adb0cc94b74594ace96e0a6b1b6f
Marcus R. Breese <marcus.breese@ucsf.edu>
2017-07-27 19:39:05 -0700

    set MALLOC_ARENA_MAX=4 by default

--- src/scripts/stub.sh
+++ src/scripts/stub.sh
@@ -6,6 +6,11 @@ fi
 
 JAVA_OPTS="${JAVA_OPTS} -Dio.compgen.cgpipe.scriptname=\"$MYSELF\""
 
+# limit MALLOC_ARENA to keep vmem under control on bigmem systems
+if [ "$MALLOC_ARENA_MAX" = "" ]; then
+    export MALLOC_ARENA_MAX=4
+fi
+
 # limit parallel gc threads (systems with lots of processors)
 FOUNDGC=0
 case "${JAVA_OPTS}" in
@@ -17,7 +22,6 @@ esac
 if [ $FOUNDGC -eq 0 ]; then
     JAVA_OPTS="${JAVA_OPTS} -XX:ParallelGCThreads=2"
 fi
-
 
 JAVABIN=$(which java)
 if [ "${JAVA_HOME}" != "" ]; then
