620ff565cb43a56e97a1b454a05e006388b3824c
Marcus R. Breese <marcus@breese.com>
2020-05-20 13:04:25 -0400

    updated tests to keep test tmp files in test/

--- test.sh
+++ test.sh
@@ -14,51 +14,58 @@ if [[ "$1" == "-v" || "$1" == "-vv" || "$1" == "-vvv" ]]; then
     shift
 fi
 
-RET=0
-
 if [ "$1" == "" ]; then
+    rm -f test/err
     find src/test-scripts -name '*.mvp' -exec $0 $VERBOSE \{\} \;
     find src/test-scripts -name '*.mvpt' -exec $0 $VERBOSE \{\} \;
+
+    if [ -e test/err ]; then
+        rm test/err
+        exit 1
+    fi
+
 else
+    RET=0
     mkdir -p test/run
     echo "global_foo = \"bar\"" > test/run/.cgpiperc
     echo "bar = \"baz\"" > test/run/global.incl
 
-    touch file1 file2
+    touch test/file1 test/file2
 
     if [ "$(echo $1 | grep ".mvpt$")" != "" ]; then
-        CGPIPE_HOME=test/run dist/cgpipe $VERBOSE -nolog -f $1 > .testout 2> .testerr
+        CGPIPE_HOME=test/run dist/cgpipe $VERBOSE -nolog -f $1 > test/.testout 2> test/.testerr
     else
-        CGPIPE_HOME=test/run $1 > .testout 2> .testerr
+        CGPIPE_HOME=test/run $1 > test/.testout 2> test/.testerr
     fi
 
-    TEST=$(cat .testout | grep -v '^#' | grep -v '^$' | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' | $MD)
+    TEST=$(cat test/.testout | grep -v '^#' | grep -v '^$' | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' | $MD)
     GOOD=$(cat $1.good | grep -v '^#' | grep -v '^$' |sed -e 's/^[[:blank:]]*//g' | $MD)
     if [ "$TEST" != "$GOOD" ]; then
         echo "$1 ERROR"
+        touch test/err
         RET=1
     else
         echo "$1 OK"
     fi
     if [ "$VERBOSE" != "" ]; then
-        cat .testout | grep -v '^#' | grep -v '^$'  | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' > .testout1
-        cat $1.good | grep -v '^#' | grep -v '^$'  | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' > .testout2
+        cat test/.testout | grep -v '^#' | grep -v '^$'  | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' > test/.testout1
+        cat $1.good | grep -v '^#' | grep -v '^$'  | sed -e 's/^[[:blank:]]*//g' | sed -e 's/CGPIPE ERROR.*/CGPIPE ERROR/g' > test/.testout2
         echo "[EXPECTED]"
-        cat .testout2
+        cat test/.testout2
         echo ""
         echo "[GOT]"
-        cat .testout1
+        cat test/.testout1
         if [ "$TEST" != "$GOOD" ]; then
             echo ""
             echo "[DIFF]"
-            diff -y .testout1 .testout2
+            diff -y test/.testout1 test/.testout2
         fi
-        rm .testout1 .testout2
+        rm test/.testout1 test/.testout2
     fi
-    rm file1 file2
-    rm .testout
-    rm .testerr
+    rm test/file1 test/file2
+    rm test/.testout
+    rm test/.testerr
     rm test/run/*
-fi
 
-exit $RET
+    exit $RET
+fi
